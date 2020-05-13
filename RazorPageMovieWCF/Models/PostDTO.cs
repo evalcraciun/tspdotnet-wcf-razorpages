24793b31a3b331eb0debcf4da3928dd8aba80c15
Ciprian Craciun <ciprian.craciun@cgm.com>
2020-05-13 17:36:49 +0300

    add Models and pages for Posts

---
+++ RazorPageMovieWCF/Models/PostDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPageMovieWCF.Models
+{
+    [DataContract]
+    public partial class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new List<CommentDTO>(); //new HashSet<Comment>();
+        }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public virtual List<CommentDTO> Comments { get; set; }
+    }
+}
